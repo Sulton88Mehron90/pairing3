2172e8fae31c91b2335edfa6453925d125f40d65
Parvin Sattorova <nanajon66@gmail.com>
2022-12-11 15:40:21 -0500

    Add another array

--- friends.js
+++ friends.js
@@ -4,3 +4,10 @@ for (var i=0; i<num.length; i++) {
         console.log(`${num[i]}`)
     }
 }
+
+var num2 = [10, 20, 30, 40, 50, 60, 70, 80]
+for (var i=0; i<num2.length; i++) {
+    if(num2[i] <=60) {
+        console.log(`${num2[i]}`)
+    }
+}
